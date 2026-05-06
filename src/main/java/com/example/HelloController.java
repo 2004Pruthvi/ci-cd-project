65a7a01d168ebde54c9c729055acf19a069f88a3
2004Pruthvi <165915900+2004Pruthvi@users.noreply.github.com>
2026-05-06 19:04:17 +0000

    new ui based

--- src/main/java/com/example/HelloController.java
+++ src/main/java/com/example/HelloController.java
@@ -1,13 +1,13 @@
 package com.example;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HelloController {
 
     @GetMapping("/")
     public String home() {
-        return "Ci/CD Pipeline Success ";
+        return "index";
     }
 }
